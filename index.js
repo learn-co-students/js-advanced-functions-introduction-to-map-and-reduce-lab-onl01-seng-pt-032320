70c959a6792e1aebfa3d4a21ab646828a0afda02
Allan5829 <allan.farca@gmail.com>
2020-10-13 18:48:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+function mapToNegativize(array) {
+    const newArray = array.map( x => x * -1)
+    return newArray
+}
+
+function mapToNoChange(array) {
+    return array
+}
+
+function mapToDouble(array) {
+    const newArray = array.map( x => x * 2)
+    return newArray
+}
+
+function mapToSquare(array) {
+    const newArray = array.map( x => x * x)
+    return newArray
+}
+
+function reduceToTotal(array, start) {
+    if (!start) {
+        start = 0
+    }
+    return array.reduce((accumulator, currentValue, currentIndex, a) => {
+        return accumulator + currentValue
+    }, start)
+}
+
+function reduceToAllTrue(array) {
+    return array.reduce((accumulator, currentValue) => {
+        if (!currentValue) {
+            return false
+        } else {
+            return true
+        }
+    })
+}
+
+function reduceToAnyTrue(array) {
+    return array.reduce((accumulator, currentValue) => {
+        if (currentValue) {
+            return true
+        } else {
+            return false
+        }
+    })
+}
