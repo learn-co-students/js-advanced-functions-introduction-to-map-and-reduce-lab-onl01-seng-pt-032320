06abd82613346eb73f9badda85c7e5e23f7edc10
VladDaImpaler18 <vladDaImpaler@gmail.com>
2020-10-24 03:39:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map(element => element * -1)
+   
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map(element => element)
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(element => element * 2)
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map( element => element**2)
+}
+
+function reduceToTotal(sourceArray, startingPoint){
+    return (sourceArray.reduce((total,element) => total+= element)) + startingPoint
+}
