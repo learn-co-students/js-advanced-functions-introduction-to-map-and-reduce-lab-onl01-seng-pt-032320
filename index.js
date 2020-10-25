f9e2cabc1a96b3774d9f56dc03c1e43b8dc0829c
vlad <VladDaImpaler@gmail.com>
2020-10-24 20:49:54 -0400

    Done.

--- index.js
+++ index.js
@@ -16,6 +16,26 @@ function mapToSquare(sourceArray){
     return sourceArray.map( element => element**2)
 }
 
-function reduceToTotal(sourceArray, startingPoint){
-    return (sourceArray.reduce((total,element) => total+= element)) + startingPoint
+function reduceToTotal(sourceArray, startingPoint=0){
+    let total = 0;
+    for(let x=0; x<sourceArray.length; x++)
+    {
+        total += sourceArray[x]
+    }    
+    total+= startingPoint;
+    return total;
+}
+function reduceToAllTrue(sourceArray){
+    for(let x=0; x<sourceArray.length; x++)
+    {
+        if(sourceArray[x] === false){ return false }
+    }
+    return true;
+}
+function reduceToAnyTrue(sourceArray){
+    for(let x=0; x<sourceArray.length; x++)
+    {
+        if(sourceArray[x] === true){ return true }
+    }
+    return false;
 }
